16e7e2c489e6cd1ffb47245460acccd2554ab0b1
NaotoUchiyama <n-uchiyama@ojs.ac.jp>
2021-05-27 14:08:36 +0900

    P63 リスト2.16

---
+++ SalesCalculator/SalesCounter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesCalculator {
+    class SalesCounter {
+        private List<Sale> _sales;
+
+        // コンストラクタ
+        public SalesCounter(List<Sale> sales) {
+            _sales = sales;
+        }
+    }
+}

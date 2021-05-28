ae938b122b9d5b6c65d59518825bfdf62bf8ebb6
NaotoUchiyama <n-uchiyama@ojs.ac.jp>
2021-05-28 14:05:39 +0900

    P77 リスト2.27_完成版

--- SalesCalculator/SalesCounter.cs
+++ SalesCalculator/SalesCounter.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace SalesCalculator {
     class SalesCounter {
-        private List<Sale> _sales;
+        private IEnumerable<Sale> _sales;
 
         // コンストラクタ
         public SalesCounter(string filePath) {
@@ -16,7 +16,7 @@ namespace SalesCalculator {
 
         // List 2-15
         // 売り上げデータを読み込み、Saleオブジェクトのリストを返す
-        private static List<Sale> ReadSales(string filePath) {
+        private static IEnumerable<Sale> ReadSales(string filePath) {
             List<Sale> sales = new List<Sale>();
             string[] lines = File.ReadAllLines(filePath);
             foreach (string line in lines) {
@@ -33,9 +33,9 @@ namespace SalesCalculator {
 
         // List 2-17
         // 店舗別売り上げを求める
-        public Dictionary<string, int> GetPerStoreSales() {
-            Dictionary<string, int> dict = new Dictionary<string, int>();
-            foreach (Sale sale in _sales) {
+        public IDictionary<string, int> GetPerStoreSales() {
+            var dict = new Dictionary<string, int>();
+            foreach (var sale in _sales) {
                 if (dict.ContainsKey(sale.ShopName))
                     //既にコレクションに店舗が設定されている
                     dict[sale.ShopName] += sale.Amount;

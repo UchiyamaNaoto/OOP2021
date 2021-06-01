1b3ed832987819db0f7e02f108338417552537c0
NaotoUchiyama <n-uchiyama@ojs.ac.jp>
2021-06-01 11:24:32 +0900

    P79 問題2.3 完成

---
+++ Exercise3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise3 {
+    class Program {
+        static void Main(string[] args) {
+            var sales = new SalesCounter("Sales.csv");
+
+            //商品カテゴリ別売り上げを求める
+            var amountPerCategory = sales.GetPerCategorySales();
+            foreach (var obj in amountPerCategory) {
+                Console.WriteLine("{0} {1:#,#}円", obj.Key, obj.Value);
+            }
+        }
+    }
+}

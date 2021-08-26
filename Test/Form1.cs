af29a208dc543cd4bf456ba4b5da23600e69987b
NaotoUchiyama <n-uchiyama@ojs.ac.jp>
2021-08-26 13:21:39 +0900

    MathクラスのPowメソッドを使用

--- Test/Form1.cs
+++ Test/Form1.cs
@@ -8,22 +8,13 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Sample0823
+namespace Test
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
-        }
-
-        private void Exec_Click(object sender, EventArgs e)
-        {
-            int num1 = int.Parse(Value.Text); //整数に変換
-            int num2 = int.Parse(Jyou.Text);
-
-            Result.Text = Math.Pow(num1, num2).ToString();
-
         }
     }
 }

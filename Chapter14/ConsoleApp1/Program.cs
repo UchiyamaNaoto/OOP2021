bbf6c74db101a142091764e976e148a24477f036
NaotoUchiyama <n-uchiyama@ojs.ac.jp>
2021-09-06 14:01:52 +0900

    演習問題完成

--- Chapter14/ConsoleApp1/Program.cs
+++ Chapter14/ConsoleApp1/Program.cs
@@ -12,69 +12,58 @@ namespace Section04
 {
     class Program
     {
-        static void Main(string[] args)
+        Dictionary<string, int> AreaDic = new Dictionary<string, int>()
         {
+            {"前橋", 4210},{"みなかみ", 4220},{"宇都宮", 4110},{"水戸", 4010},{"さいたま", 4310},
+        };
+
+        //コードを保存する
+        List<int> cityCode = new List<int>();
+
+        static void Main(string[] args){
             new Program();
         }
 
         //コンストラクタ
         public Program()
         {
-            //DownloadString();
-            //DownloadFileAsync();
-            //OpenReadSample();
-            var results = GetWeatherReportFromYahoo(4210);
+            Console.WriteLine("yahoo！週間天気予報");
+            Console.WriteLine(); //改行
+            Console.WriteLine("地域コードを入力");
+
+            int num = 1;
+            foreach (KeyValuePair<string,int> pair in AreaDic)
+            {
+                Console.WriteLine("{0}:{1}", num++, pair.Key );
+                cityCode.Add(pair.Value);   //コードをリストへ保存
+            }
+
+            Console.WriteLine("9:その他（直接入力）");
+            Console.WriteLine(); //改行
+
+            Console.Write(">");
+            //文字列として入力した数字を取り込む
+            var selectArea = Console.ReadLine();
+            int pos = int.Parse(selectArea);
+
+            int code;
+            if (pos != 9)
+            {
+                code = cityCode[pos - 1];
+            }
+            else
+            {
+                //その他の場合
+                Console.Write("都市コードを入力：");
+                code = int.Parse(Console.ReadLine());                
+            }
+
+            var results = GetWeatherReportFromYahoo(code);
             foreach (var s in results)
             {
                 Console.WriteLine(s);
             }
             Console.ReadLine(); //入力待ち 
-        }
-
-        //リスト14.15
-        public void DownloadString()
-        {
-            var wc = new WebClient();
-            wc.Encoding = Encoding.UTF8;
-            var html = wc.DownloadString("https://yahoo.co.jp/");
-            Console.WriteLine(html);
-        }
-
-        //リスト14.17（非同期処理）
-        private void DownloadFileAsync()
-        {
-            var wc = new WebClient();
-            var url = new Uri(@"C:\Users\n_uchiyama\Desktop\download.zip");
-            var filename = @"C:\temp\example.zip";
-            wc.DownloadProgressChanged += wc_DownloadProgressChanged;
-            wc.DownloadFileCompleted += wc_DownloadFileCompleted;
-            wc.DownloadFileAsync(url, filename); //非同期でダウンロード
-            Console.ReadLine();//アプリケーションが終了しないようにする
-        }
-
-        static void wc_DownloadProgressChanged(object sender,
-                            DownloadProgressChangedEventArgs e)
-        {
-            Console.WriteLine("{0}% {1}/{2}", e.ProgressPercentage,
-                              e.BytesReceived, e.TotalBytesToReceive);
-        }
-
-        static void wc_DownloadFileCompleted(object sender,
-                            System.ComponentModel.AsyncCompletedEventArgs e)
-        {
-            Console.WriteLine("ダウンロード完了");
-        }
-
-        //リスト14.18（ストリームとしてダウンロード）
-        public void OpenReadSample()
-        {
-            var wc = new WebClient();
-            using (var stream = wc.OpenRead(@"https://yahoo.co.jp/"))
-            using (var sr = new StreamReader(stream, Encoding.UTF8))
-            {
-                string html = sr.ReadToEnd();
-                Console.WriteLine(html);
-            }
         }
 
         //リスト14.19
---
+++ WeatherApp/WeatherApp/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿
+namespace WeatherApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(110, 30);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "地域を選択";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(247, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "天気表示";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(44, 61);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(323, 268);
+            this.textBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(421, 353);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+

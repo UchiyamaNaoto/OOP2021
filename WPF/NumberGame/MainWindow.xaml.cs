f8f9479bc195ea876627b35aeef08e78dc9a0de2
NaotoUchiyama <n-uchiyama@ojs.ac.jp>
2021-11-12 15:59:02 +0900

    NumberGame動的

---
+++ WPF/NumberGame/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NumberGame
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            List<Button> buttons = new List<Button>();
+
+            for (int i = 0; i < 5; i++)
+            {                
+                for (int j = 0; j < 5; j++)
+                {
+                    var bt = new Button();
+                    bt.Width = 110;
+                    bt.Height = 100;
+                    bt.Content = (j + 1) + (i * 5);
+                    bt.FontSize = 20;
+                    Grid.SetRow(bt, i);
+                    Grid.SetColumn(bt, j);
+                    buttons.Add(bt);
+                }
+            }
+            buttons.ForEach(bt => grid.Children.Add(bt));
+        }
+    }
+}
